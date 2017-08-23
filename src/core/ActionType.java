a18a672c5bcbd4592546cf49a771f10d4f88a069
Ryan P. Clark <universalexports2013@gmail.com>
2017-08-23 14:44:55 -0700

    Remove core and render packages

--- src/core/ActionType.java
+++
@@ -1,9 +0,0 @@
-package core;
-
-/**
- * Created by rycla on 12/19/2016.
- */
-public enum ActionType {
-    NEW_CHAR, NEW_LINE, BACKSPACE, DELETE, GROUP_DELETE, GROUP_REPLACE_WITH_LINE, GROUP_REPLACE_WITH_CHAR,
-    PASTE_NO_REPLACE, PASTE_AND_REPLACE
-}
